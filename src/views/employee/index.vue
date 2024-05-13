6daba971921b013e763b3be0d95a809620fa0ced
Lin <1030863031@qq.com>
2024-05-14 02:11:45 +0800

    完成员工树状图渲染及部门id获取

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -1,12 +1,99 @@
 <template>
   <div class="container">
     <div class="app-container">
-      员工
+      <div class="left">
+        <el-input style="margin-bottom:10px" type="text" prefix-icon="el-icon-search" size="small" placeholder="输入员工姓名全员搜索" />
+        <!-- 树形组件 -->
+        <el-tree
+          ref="deptTree"
+          node-key="id"
+          :expand-on-click-node="false"
+          highlight-current
+          default-expand-all
+          :data="deptList"
+          :props="defaultProps"
+          @current-change="selectNode"
+        />
+      </div>
+      <div class="right">
+        <el-row class="opeate-tools" type="flex" justify="end">
+          <el-button size="mini" type="primary">添加员工</el-button>
+          <el-button size="mini">excel导入</el-button>
+          <el-button size="mini">excel导出</el-button>
+        </el-row>
+        <!-- 表格组件 -->
+        <!-- 分页 -->
+      </div>
     </div>
   </div>
 </template>
+
 <script>
+import { depGetDepartment } from '@/api/department'
+import { transListToTreeData } from '@/utils'
 export default {
-  name: 'Employee'
+  name: 'Employee',
+  data() {
+    return {
+      deptList: {}, //  部门数据
+      defaultProps: {
+        label: 'name',
+        children: 'children'
+      },
+      queryParams: {
+        departmentId: null, //  部门id
+        page: 1, //  当前页码数
+        pagesize: 5 //  一页的数据数量
+      }
+    }
+  },
+  created() {
+    this.getDepartment()
+  },
+  methods: {
+    async getDepartment() {
+      this.deptList = transListToTreeData(await depGetDepartment(), 0)
+      this.queryParams.departmentId = this.deptList[0].id
+      this.$nextTick(() => {
+        // 设置选中节点
+        this.$refs.deptTree.setCurrentKey(this.queryParams.departmentId)
+      })
+    },
+    // 选中节点，记录 id
+    selectNode(node) {
+      this.queryParams.departmentId = node.id
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.app-container {
+  background: #fff;
+  display: flex;
+  .left {
+    width: 280px;
+    padding: 20px;
+    border-right: 1px solid #eaeef4;
+  }
+  .right {
+    flex: 1;
+    padding: 20px;
+    .opeate-tools {
+      margin:10px ;
+    }
+    .username {
+      height: 30px;
+      width: 30px;
+      line-height: 30px;
+      text-align: center;
+      border-radius: 50%;
+      color: #fff;
+      background: #04C9BE;
+      font-size: 12px;
+      display:inline-block;
+    }
+  }
+}
+
+</style>
