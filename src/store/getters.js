741d2e0f3746015dd7d849c4a29ad2f4d2490e50
Lin <1030863031@qq.com>
2024-05-18 15:30:02 +0800

    完成首页个人信息渲染

--- src/store/getters.js
+++ src/store/getters.js
@@ -5,6 +5,8 @@ const getters = {
   userId: state => state.user.userInfo.userId,
   avatar: state => state.user.userInfo.staffPhoto, //  头像
   name: state => state.user.userInfo.username, //  用户名称
+  company: state => state.user.userInfo.company, //  公司名称
+  departmentName: state => state.user.userInfo.departmentName, //  部门名称
   routes: state => state.user.routes //  路由信息
 }
 // getters 便捷访问
